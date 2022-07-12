72979571830ba91de4e64b507b2f4bcdb3a8fcce
stkitziris <stkitziris@gmail.com>
2022-07-12 16:23:28 +0300

    fix types in services

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosResponse } from 'axios';
-import { Product, ProductsResponse } from '../types';
+import { ProductResponse, ProductsResponse } from '../types';
 
 export const getProducts = async (page: number): Promise<ProductsResponse> => {    
     const { data }: AxiosResponse<ProductsResponse> = await axios.get('/api/products', {
@@ -10,7 +10,7 @@ export const getProducts = async (page: number): Promise<ProductsResponse> => {
     return data;
 }
 
-export const getProduct = async (id: string | string[]): Promise<Product> => {    
-    const { data }: AxiosResponse<Product> = await axios.get(`/api/products/${id}`);
+export const getProduct = async (id: string | string[]): Promise<ProductResponse> => {    
+    const { data }: AxiosResponse<ProductResponse> = await axios.get(`/api/products/${id}`);
     return data;
 }
